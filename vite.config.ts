4d6d6dfacd79bc8d4aa4ff6edc58a3f55eaeeb88
Huang Weijie <h_weijie@yahoo.com>
2022-04-08 21:56:37 +0800

    fix: fix opus recording cjs issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), nodePolyfills({ include: null })],
+  plugins: [svelte()],
 });
